0277f0a23981d2438e2ccb2375088a9d6f7c4b12
Adm <Adm@Adm-PC>
2011-05-16 23:11:43 -0300

    [no commit message]

--- src/negocio/Mensagens.java
+++ src/negocio/Mensagens.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package negocio;
 
 import java.util.Scanner;
@@ -20,21 +19,51 @@ import tabuleiro.Tabuleiro;
  * @interface Mensagens contem as assinaturas dos métodos utilizados por MensagensJogo para exibir
  * as principais mensagens do sistema.
  */
-
-
 public interface Mensagens {
 
+    /**
+     * Metodo para mostrar mensagesn de boas vindas ao usuario
+     *
+     */
     public String mensagensBoasVindas();
 
-
+    /**
+     * Metodo para mostrar mensagem de tetativas de execucoes erradas.
+     * @param nivelBurrice
+     * @param numDigitado
+     */
     public String nivelBurrice(int nivelBurrice, int numDigitado);
 
+    /**
+     * Metodo para mostrar a mensagem inicial pedindo o nome do jogador
+     * @param numJogador
+     * @param  teclado
+     *
+     * @return  String
+     */
     public String mensagemNome(int numJogador, Scanner teclado);
 
+    /**
+     * Metodo para mostrar as mensagem com as cores disponiveis no jogo
+     * @param jogador Jogador,
+     * @param cores
+     * @param numJogador
+     * @param teclado
+     * @return String
+     */
     public String mensagemCores(Jogador jogador, String[] cores, int numJogador, Scanner teclado);
 
-
+    /**
+     * * Metodo para mostrar em que posicao está a propriedade e pegunta se o jogador deseja comprá-la
+     * @param j
+     * @param l
+     */
     public void geraStatus(Jogador j, LugarFisico l);
 
+    /**
+     * Metodo para mostrar a situacao atual do jogador
+     * @param j
+     * @param t
+     */
     public void statusJogador(Jogador j, Tabuleiro t);
 }
